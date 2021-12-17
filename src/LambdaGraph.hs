73315fc87af99f4632d2961730d205da76dcdc97
smudgecat123 <jf14218@my.bristol.ac.uk>
2021-12-17 14:55:09 +0000

    slow and steady

--- src/LambdaGraph.hs
+++ src/LambdaGraph.hs
@@ -8,51 +8,51 @@ import qualified Data.Set as Set
 
 ----------------------------------------------------------------------
 
---a graph is a hashmap with associated metadata to allow constant time insert at first available key, plus the index of the top-level node
-data Graph' a = Graph' {map :: Map a (Node a), next :: a, recycle :: [a], top :: a}
+data Graph k = Graph {map :: Map k (Node k), next :: k, recycle :: [k], top :: k}
 
---if k == Nothing, use first available key
-insert :: Maybe a -> Node a -> Graph' a -> Graph' a
-insert k n g = undefined
+readG :: k -> Graph k -> Node k
+readG k g = undefined
 
-read :: a -> Graph' a -> Node a
-read k g = undefined
+insertG :: Node k -> Graph k -> Graph k
+insertG n g = undefined
 
---this should throw an error if we attempt to delete a non-existant key, otherwise recycle could end up with duplicates
-delete :: a -> Graph' a -> Graph' a
-delete k g = undefined
+updateG :: k -> Node k -> Graph k -> Graph k
+updateG k n g = undefined
+
+deleteG :: k -> Graph k -> Graph k
+deleteG k g = undefined
 
 ----------------------------------------------------------------------
 
-type Graph a = Map a (Node a)
-
---unidirectional
--- data Node a = Var {bin :: a}
---             | App {dat :: a, fun :: a}
---             | Bin {bod :: a}
-
 --bidirectional
-data Node a = Var {up :: a, bin :: a}
-            | App {up :: a, datum :: a, func :: a}
-            | Bin {up :: a, body :: a, vars :: [a]}
+data Node k = Var {bind :: k}
+            | App {arg :: k, func :: k}
+            | Bin {body :: k, vars :: [k]}
 
-type Term a = a
+newtype Term k = Term {unTerm :: k}
+
+program :: Graph k -> Term k
+program = Term . top
 
 --eliminator for Terms
-term :: Ord a => Graph a -> (a -> b) -> (b -> b -> b) -> (b -> [a] -> b) -> Term a -> b
-term g v a b t = case g Map.! t of
-  Var _ vb -> v vb
-  App _ ad af -> a (term g v a b ad) (term g v a b af)
-  Bin _ bb bv -> b (term g v a b bb) bv
+term :: Ord k => Graph k -> (k -> a) -> (a -> a -> a) -> (a -> [k] -> a) -> Term k -> a
+term g v a b t = case readG (unTerm t) g of
+  Var vb -> v vb
+  App aa af -> a (term g v a b (Term aa)) (term g v a b (Term af))
+  Bin bb bv -> b (term g v a b (Term bb)) bv
 
-tSize :: Ord a => Graph a -> Term a -> Int
+tSize :: Ord k => Graph k -> Term k -> Int
 tSize g = term g (const 1) (+) const
 
-tVars :: Ord a => Graph a -> Term a -> Set a
+tVars :: Ord k => Graph k -> Term k -> Set k --this is wrong, singleton gives us the key of the binder for the variable
 tVars g = term g Set.singleton Set.union const
 
-tBound :: Ord a => Graph a -> Term a -> Set a
+tBound :: Ord k => Graph k -> Term k -> Set k
 tBound g = term g (const Set.empty) Set.union (\s ls -> s `Set.union` Set.fromList ls)
 
-tFree :: Ord a => Graph a -> Term a -> Set a
+tFree :: Ord k => Graph k -> Term k -> Set k
 tFree g t = tVars g t Set.\\ tBound g t
+
+dup :: Graph k -> Term k -> k -> Graph k
+dup g t k = undefined
+
