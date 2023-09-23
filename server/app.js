0cf473e847670ff59595bfeb99a63a5658c9b581
smudgecat123 <jf14218@my.bristol.ac.uk>
2023-09-23 20:17:13 +0100

    added loop prevention

--- server/app.js
+++ server/app.js
@@ -7,10 +7,14 @@ const port = 3000;
 
 app.use(bodyParser.text());
 
+//this loops: 'stack run -- -e "(\f -> (\x -> f (x x)) (\x -> f (x x))) F"'
+//so we have to add a timeout:
+const timeout = 2;
+
 app.post('/reduce', (req, res) => {
   let expr = req.body;
   //console.log(`Recieved Expr: ${expr}`)
-  let command = `../.stack-work/dist/x86_64-linux-tinfo6/Cabal-3.2.1.0/build/circuit-exe/circuit-exe -e "${expr}"`
+  let command = `timeout ${timeout}s ../.stack-work/dist/x86_64-linux-tinfo6/Cabal-3.2.1.0/build/circuit-exe/circuit-exe -e "${expr}"`
   exec(command, (error, stdout, stderr) => {
     if (error) {
       return res.status(500).send('Error executing the command.');
